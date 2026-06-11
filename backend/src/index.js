033fd5cc41c136f6515990817336b054ac2e7baa
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-06-11 14:51:47 +0100

    Implement user authentication routes and book management functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import "dotenv/config";
 
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 
-const app = express();
+import { connectDB } from "./lib/db.js";
+
+
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello server"));
 
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
